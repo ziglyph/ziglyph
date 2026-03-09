dea5c3d3cd396167516aefde61422cf708b876f7
Tony Sidrock <tonysidrock@gmail.com>
2026-03-09 07:00:26 +0000

    docs:

--- cli/main.zig
+++ cli/main.zig
@@ -13,10 +13,12 @@ pub fn main() !void {
     }
 
     const cmd = args[1];
-    if (std.mem.eql(u8, cmd, "skeleton")) {
+    if (std.mem.eql(u8, cmd, "skeleton") or std.mem.eql(u8, cmd, "s")) {
         try run_skeleton(allocator);
-    } else if (std.mem.eql(u8, cmd, "detector")) {
-        try run_skeleton(allocator);
+    } else if (std.mem.eql(u8, cmd, "normalizer") or std.mem.eql(u8, cmd, "n")) {
+        try run_normalizer(allocator);
+    } else if (std.mem.eql(u8, cmd, "detector") or std.mem.eql(u8, cmd, "d")) {
+        try run_detector(allocator);
     } else {
         std.debug.print("Unknown command: {s}\n", .{cmd});
         print_usage();
@@ -42,6 +44,39 @@ fn run_skeleton(allocator: std.mem.Allocator) !void {
     std.debug.print("Running skeleton...\n", .{});
     const input = "раурвl"; // contains Cyrillic letters
     const result = try sk.compute(input);
+
+    std.debug.print(
+        \\{s}
+        \\{s}
+        \\
+    , .{ result, "paypal" });
+    std.debug.print("Skeleton finished.\n", .{});
+}
+
+fn run_normalizer(allocator: std.mem.Allocator) !void {
+    var nm = zgl.normalizer.Normalizer.init(allocator);
+    defer nm.deinit();
+
+    std.debug.print("Running skeleton...\n", .{});
+    // ℓ = script small l
+    const input = "paypaℓ";
+    const result = try nm.nfkc(input);
+
+    std.debug.print(
+        \\{s}
+        \\{s}
+        \\
+    , .{ result, "paypal" });
+    std.debug.print("Skeleton finished.\n", .{});
+}
+
+fn run_detector(allocator: std.mem.Allocator) !void {
+    var dt = zgl.detector.Detector.init(allocator);
+    defer dt.deinit();
+
+    std.debug.print("Running skeleton...\n", .{});
+    const input = "раурвl"; // contains Cyrillic letters
+    const result = try dt.detect(input);
 
     std.debug.print(
         \\{s}
